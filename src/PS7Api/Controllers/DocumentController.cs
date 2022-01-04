8e3c8bf7bb4732c98b6ddf8239028bdf778c76a2
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-01-04 10:21:22 +0100

    Example controller

---
+++ src/PS7Api/Controllers/DocumentController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PS7Api.Models;
+
+namespace PS7Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        private readonly ILogger<DocumentController> _logger;
+        private readonly Ps7Context _context;
+
+        public DocumentController(ILogger<DocumentController> logger, Ps7Context context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        
+        // POST: api/Document
+        [HttpPost(Name = "Scan")]
+        public async Task<IActionResult> Scan(IFormFile file)
+        {
+            _logger.LogDebug("Scanning document size {Len}", file.Length);
+            
+            var ms = new MemoryStream();
+            await file.CopyToAsync(ms);
+            var document = new Document() { Image = ms.ToArray() };
+            
+            _context.Documents.Add(document);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Get", new { id = document.Id }, document);
+        }
+
+        // GET: api/Document/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var doc = await _context.Documents.FindAsync(id);
+                
+            if (doc == null)
+            {
+                return NotFound();
+            }
+                
+            return Ok(doc);
+        }
+
+        // DELETE: api/Document/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var doc = await _context.Documents.FindAsync(id);
+            
+            if (doc == null)
+            {
+                return NotFound();
+            }
+            
+            _context.Documents.Remove(doc);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
