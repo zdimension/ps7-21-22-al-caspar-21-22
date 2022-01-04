accadb3c50e2c3fcf49966e4bb51751fdec29160
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2022-01-04 14:48:48 +0100

    Add tests #15
    for action on non existing document and empty list

---
+++ tests/UnitTests/Controllers/DocumentAnomalyControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PS7Api.UnitTests.Controllers;
+
+public class DocumentAnomalyControllerTests
+{
+	[Fact]
+	public async Task Empty_DocumentAnomaly_GET_Returns_200()
+	{
+		await using var app = new Ps7Fixture();
+
+		var client = app.CreateClient();
+		var allDocAno = await client.GetAsync("/api/DocumentAnomaly");
+		
+		Assert.Equal(HttpStatusCode.OK, allDocAno.StatusCode);
+	}
+	
+	[Fact]
+	public async Task Missing_DocumentAnomaly_GET_Returns_404()
+	{
+		await using var app = new Ps7Fixture();
+
+		var client = app.CreateClient();
+		var docAno = await client.GetAsync("/api/DocumentAnomaly/0");
+		
+		Assert.Equal(HttpStatusCode.NotFound, docAno.StatusCode);
+	}
+	
+	[Fact]
+	public async Task Missing_DocumentAnomaly_DELETE_Returns_404()
+	{
+		await using var app = new Ps7Fixture();
+
+		var client = app.CreateClient();
+		var docAno = await client.DeleteAsync("/api/DocumentAnomaly/0");
+		
+		Assert.Equal(HttpStatusCode.NotFound, docAno.StatusCode);
+	}
+}
