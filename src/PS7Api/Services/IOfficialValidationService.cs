43e72881f58714861f7fb3585637c43767d5a207
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-01-05 16:57:13 +0100

    Add govt API interface

---
+++ src/PS7Api/Services/IOfficialValidationService.cs
@@ -0,0 +1,12 @@
+﻿using PS7Api.Models;
+
+namespace PS7Api.Services;
+
+public interface IOfficialValidationService
+{
+    ValidationResult ValidateDocument(Document document);
+}
+
+public abstract record ValidationResult;
+public record ValidationSuccess : ValidationResult;
+public record ValidationFailure(string[] Errors) : ValidationResult;
