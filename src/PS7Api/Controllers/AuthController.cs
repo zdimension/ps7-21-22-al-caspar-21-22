6b966e541eafb8d8d1ef7e11e7231f5732867cd2
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2022-01-15 16:57:22 +0100

    Remove erroneous using

--- src/PS7Api/Controllers/AuthController.cs
+++ src/PS7Api/Controllers/AuthController.cs
@@ -2,7 +2,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Google.Authenticator;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
